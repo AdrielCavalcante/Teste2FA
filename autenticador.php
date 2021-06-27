eff402eb4205b1a0818aa936eba9c565463ec6b0
Adriel Cavalcante <72228497+AdrielCavalcante@users.noreply.github.com>
2021-06-27 17:29:49 -0300

    Add test

---
+++ autenticador.php
@@ -0,0 +1,42 @@
+<?php
+//Criando o form para conferir se o Token de 6 digitos está correto
+include_once('../vendor/sonata-project/google-authenticator/src/FixedBitNotation.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleAuthenticatorInterface.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleAuthenticator.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleQrUrl.php');
+
+$g = new \Google\Authenticator\GoogleAuthenticator();
+
+//Criar chave secret
+$secret = 'AdrielPegadorDeTodas';
+
+//Validar o Token enviado
+if(isset($_POST['token'])){
+    $token = $_POST['token'];
+    if($g->checkCode($secret, $token)){
+        //true
+        echo 'Autenticação concluida com sucesso';
+    } else {
+        //false
+        echo 'Token inválido ou expirado';
+    }
+    die(); //Só pra n iniciar o form dnv
+}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GoogleAuth</title>
+</head>
+<body>
+    <h1>Informe o Token de autenticação 2FA</h1>
+    <form method="post">
+        <input type="number" name="token" />
+        <button type="submit">Autenticar</button>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//Criando QR Code para ler com o app
+include_once('../vendor/sonata-project/google-authenticator/src/FixedBitNotation.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleAuthenticatorInterface.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleAuthenticator.php');
+include_once('../vendor/sonata-project/google-authenticator/src/GoogleQrUrl.php');
+
+$g = new \Google\Authenticator\GoogleAuthenticator();
+
+//Criar chave secret
+$secret = 'AdrielPegadorDeTodas';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Auth</title>
+</head>
+<body>
+    <h1>Resgistre sua autenticação de 2 fatores com o GoogleAuthenticator</h1>
+    <a href="autenticador.php">Inserir o token</a>
+    <img src="<?php echo $g->getUrl('NoteSec', 'URL HOSPEDADA', $secret)?>" alt="Qr Code" />
+</body>
+</html>
