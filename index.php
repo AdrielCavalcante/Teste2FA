1c41e79694611bdb5bd85c894948e486296d03d9
Adriel Cavalcante <72228497+AdrielCavalcante@users.noreply.github.com>
2021-06-28 08:41:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GoogleAuth</title>
-</head>
-<body>
-    <h1>Informe o Token de autenticação 2FA</h1>
-    <form method="post">
-        <img src="<?php echo $g->getUrl('', 'teste-2fa-google-php.herokuapp.com', $secret)?>" alt="Qr Code" />
-        <input type="number" name="token" />
-        <button type="submit">Autenticar</button>
-    </form>
-</body>
-</html>
-
 <?php
 
 include_once('vendor/sonata-project/google-authenticator/src/FixedBitNotation.php');
